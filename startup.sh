cf683efde5cdc3d996e2af4901235999f39d5199
Chris Collins <christopher.collins@duke.edu>
2014-05-12 10:19:43 -0400

    adding startup script to run Drush site-install on container start, if it has not been done already

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DB_FILE="/db_file.txt"
+
+# Check to see if Drupal is already installed
+if [ ! -f "/var/www/html/sites/default/settings.php" ] ; then
+  /bin/echo "Drush installing Drupal"
+  yes | drush site-install --db-url="$(cat DB_FILE)" -r /var/www/html
+  /bin/echo "0 * * * * /usr/bin/php /var/www/html/cron.php" >> /etc/cron.d/drupal.cron
+fi
+
+/bin/echo "Starting init system"
+/sbin/runsvdir-start

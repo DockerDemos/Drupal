a6ed42d2465d661c2872b5de797eb34c7e4e2212
Chris Collins <christopher.collins@duke.edu>
2014-05-12 10:34:45 -0400

    fixed missing variable for DB file

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ DB_FILE="/db_file.txt"
 # Check to see if Drupal is already installed
 if [ ! -f "/var/www/html/sites/default/settings.php" ] ; then
   /bin/echo "Drush installing Drupal"
-  yes | drush site-install --db-url="$(cat DB_FILE)" -r /var/www/html
+  yes | drush site-install --db-url="$(cat $DB_FILE)" -r /var/www/html
   /bin/echo "0 * * * * /usr/bin/php /var/www/html/cron.php" >> /etc/cron.d/drupal.cron
 fi
 
